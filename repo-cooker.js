ec8edb6812bf5f0774ba5407692598f94fc803d4
Henri Hulski <henri.hulski@gazeta.pl>
2019-05-18 14:45:23 +0200

    feat(monorepo): add option to use an existing GitHub token

--- repo-cooker.js
+++ repo-cooker.js
@@ -1,5 +1,12 @@
 import { Cooker } from 'repo-cooker'
 
+/**
+ * If you want to use an existing GitHub personal access token
+ * for Travis (GH_TOKEN in this case) just uncomment the next line
+ */
+
+// process.env.REPO_COOKER_GITHUB_TOKEN = process.env.GH_TOKEN
+
 export const cooker = Cooker(process.argv, {
   devtools: {
     host: 'localhost:8787, reconnect: false',
